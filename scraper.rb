18b4d6a793768751216dcddbd0a09cbc309553c3
Matthew Landauer <matthew@oaf.org.au>
2015-03-18 11:36:54 +1100

    First pass at scraper

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,28 @@
-# This is a template for a Ruby scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'mechanize'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+def extract_table_contents(t)
+  t.search("tr").map do |tr|
+    tr.search("td").map {|td| td.inner_text.strip}
+  end
+end
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+agent = Mechanize.new
+
+page = agent.get("http://nsw.greens.org.au/councillors-by-council")
+
+t = extract_table_contents(page.at("table"))
+
+t[1..-1].each do |row|
+  # Order of data: "Council or Shire", "Councillor Name", "Phone", "Email", "Ward"
+  p row
+  raise "Unexpected number of items" unless row.count == 5
+  record = {
+    "council" => row[0],
+    "name" => row[1],
+    "phone" => row[2],
+    "email" => row[3],
+    "ward" => row[4]
+  }
+  ScraperWiki.save_sqlite(["council", "name"], record)
+end
